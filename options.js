8eac5102f17c9882c5314d984ae7289e2c7e3145
Rohit Shakya <cs18b029@iittp.ac.in>
2022-01-29 17:47:43 +0530

    delete project functionality added

--- options.js
+++ options.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    var tabsData=[];
+    var tabsData = [];
 
     chrome.storage.sync.get('savetabs', function (data) {
 
@@ -13,15 +13,15 @@ $(function () {
             projectsData = jsonData;
 
             let text = "";
-            let projectText="";
+            let projectText = "";
 
             projectsData.forEach(function (project, index) {
                 tabsData.push(project);
 
-                projectText+=`<option value="${index}">${project.projectName}</option>`
+                projectText += `<option value="${index}">${project.projectName}</option>`
 
-                text += `<div class="col-auto card" id="project">
-                <div class="row m-3">
+                text += `<div class="col-auto card mx-2 my-3" id="project">
+                <div class="row mt-3">
             <div class="col-6">
                 <h3>${project.projectName}</h3>
             </div>
@@ -36,7 +36,7 @@ $(function () {
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
-                <tbody id="fillTabs">
+                <tbody id="projectBody">
                     `
                 project.projectData.forEach(function (tab, index) {
                     text += `<tr>
@@ -52,12 +52,12 @@ $(function () {
                 text += `
                 </tbody>
             </table>
-            <div class="row mb-3">
+            <div class="row mb-3" id="projectBottom">
                 <div class="col-6">
                     <input type="text" class="form-control" id="tabUrl" placeholder="Enter URL">
                 </div>
                 <div class="col-6">
-                    <input id="addTab" type="submit" class="btn btn-primary" value="add">
+                    <button id="addTab" class="btn btn-primary">add</button>
                 </div>
             </div>
         </div>`
@@ -69,13 +69,7 @@ $(function () {
         }
     });
 
-    // $('#p0').click(function () {
-    //     tabsData.forEach(function(tab, index){
-    //         console.log(tab.projectName);
-    //     })
-    // });
-
-    $('#resetTotal').click(function () {
+    $('#addProject').click(function () {
         var projectId = $('#fillProjectsName').val();
         projectId = parseInt(projectId);
 
@@ -83,4 +77,64 @@ $(function () {
             window.open(tab.url);
         });
     });
+
+    $('#deleteProject').click(function () {
+        var projectId = $('#fillProjectsName').val();
+        projectId = parseInt(projectId);
+
+        chrome.storage.sync.get('savetabs', function (data) {
+
+            if (data.savetabs) {
+
+                var jsonData = JSON.parse(data.savetabs);
+
+                var spliced = jsonData.splice(projectId, 1);
+
+                chrome.storage.sync.set({ 'savetabs': JSON.stringify(jsonData) }, function () {
+
+                    var notifOptions = {
+                        type: 'basic',
+                        iconUrl: 'icon48.png',
+                        title: spliced[0].projectName + " deleted!",
+                        message: spliced[0].projectName + " deleted successfully!"
+                    }
+
+                    chrome.notifications.create('limitNotification', notifOptions);
+
+                });
+            }
+        });
+    });
+
+    // $('#addTab').click(function () {
+    //     // var projectId = $('#fillProjectsName').val();
+    //     // projectId = parseInt(projectId);
+
+    //     $.ajax({
+    //         url: "http://textance.herokuapp.com/title/https://github.com/r-shakya/tool-development/blob/master/GIP/templates/index.html",
+    //         complete: function (data) {
+
+    //             var notifOptions = {
+    //                 type: 'basic',
+    //                 iconUrl: 'icon48.png',
+    //                 title: data.responseText + " deleted!",
+    //                 message: data.responseText + " deleted successfully!"
+    //             }
+
+    //             chrome.notifications.create('limitNotification', notifOptions);
+    //         }
+    //     });
+
+    //     // chrome.storage.sync.get('savetabs', function (data) {
+
+    //     //     if (data.savetabs) {
+
+    //     //         var jsonData = JSON.parse(data.savetabs);
+
+    //     //         chrome.storage.sync.set({ 'savetabs': JSON.stringify(jsonData) }, function () {
+
+    //     //         });
+    //     //     }
+    //     // });
+    // });
 });
