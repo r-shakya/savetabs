672d8f150e5b489842745ef16979471a46a0617d
r-shakya <cs18b029@iittp.ac.in>
2022-02-02 22:22:13 +0530

    added open and delete functionality in popup page

--- popup.js
+++ popup.js
@@ -20,6 +20,117 @@ $(function () {
         //console.log("tabsData", tabsData);
     });
 
+    var projects = [];
+
+    chrome.storage.sync.get('savetabs', function (data) {
+
+        if (data.savetabs) {
+
+            var jsonData = JSON.parse(data.savetabs);
+
+            projects = jsonData;
+
+            let projectText = "";
+
+            projects.forEach(function (project, index) {
+
+                projectText += `<tr>
+                <td>${project.projectName}</td>
+                <td>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="project-${index}">
+                        <label class="form-check-label" for="${index}">mark</label>
+                    </div>
+                </td>
+            </tr>`
+
+            });
+
+            document.getElementById('openTabs').innerHTML = projectText;
+        }
+    });
+
+    $('#openProject').click(function () {
+
+        var projectToBeOpened = [];
+
+        projects.forEach(function (project, index) {
+            var checkbox = document.getElementById("project-" + index);
+            if (checkbox.checked) {
+                projectToBeOpened.push(project);
+            }
+        });
+
+        projectToBeOpened.forEach(function (project, index) {
+
+            chrome.windows.create({}, function(wdata){
+
+                project.projectData.forEach(function (tab, index1) {
+
+                    chrome.tabs.create({windowId: wdata.id, url: tab.url, index: 0},function(data){
+    
+                    });
+
+                });
+
+            });
+
+        });
+
+    });
+
+    $('#deleteProject').click(function () {
+
+        var ProjectsRemained = [];
+        let text = "";
+
+        projects.forEach(function (project, index) {
+            var checkbox = document.getElementById("project-" + index);
+            if (!checkbox.checked) {
+                ProjectsRemained.push(project);
+            }
+            else{
+                text += project.projectName + " ";
+            }
+        });
+
+        chrome.storage.sync.set({ 'savetabs': JSON.stringify(ProjectsRemained) }, function () {
+
+            var notifOptions = {
+                type: 'basic',
+                iconUrl: 'icon48.png',
+                title: "deleted successfully!",
+                message: text + "deleted successfully!"
+            }
+
+            chrome.notifications.create('limitNotification', notifOptions);
+
+        });
+
+        // var highIndex = -1;
+
+        // chrome.windows.getAll(function(data){
+        //     data.forEach(function(w, i){
+        //         if(w.id > highIndex){
+        //             highIndex = w.id;
+        //         }
+        //     })
+        //     console.log(data);
+        //     console.log("high index: ", highIndex);
+        // });
+          
+
+
+        // var projectId = $('#fillProjectsName').val();
+        // projectId = parseInt(projectId);
+
+        // tabsData[projectId].projectData.forEach(function (tab, index) {
+        //     window.open(tab.url);
+        // });
+    });
+
+    
+
     $('#saveProject').click(function () {
 
         var dataToBeSaved = [];
